1ea1506651daba86b2ef100bfd1406f885864c8f
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-24 11:56:52 +0100

    Button register grid size

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector (".grid");
+const sizeBtn = document.querySelector ("button");
 
 for (let i = 0; i < 16; i += 1) {
     const gridSquare = document.createElement ("gridSquare");
@@ -6,9 +7,13 @@ for (let i = 0; i < 16; i += 1) {
 
     gridSquare.addEventListener ("mouseenter", (e) => {
         gridSquare.style.background = "blue";
-        console.log(e.target);
     });
 }
+
+sizeBtn.addEventListener ("click", (e) => {
+    const gridSize = prompt ("Please enter the size of the grid.")
+    console.log(gridSize);
+})
 
 /* Create grid by user input
 try to use a loop in a function
